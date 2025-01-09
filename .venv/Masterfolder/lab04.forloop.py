0870aae4df7cd596905e804c05ccec422e3a9a4d
AKANSHA <akansha.test17@gmail.com>
2025-01-09 19:17:55 +0530

    code

---
+++ .venv/Masterfolder/lab04.forloop.py
@@ -0,0 +1,19 @@
+for i in range (0,10,1):
+    if i==4:
+        print("four")
+    else:
+        pass
+
+
+
+for i in range(0,101):
+    if i%2==0:
+        print(i)
+    else:
+        pass
+
+    for i in range(0, 101):
+        if i % 2 == 0:
+            continue
+        else:
+            print(i)
