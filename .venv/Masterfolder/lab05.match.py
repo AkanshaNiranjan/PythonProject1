c2584ec09da6613932d0079e1489eefb76161649
AKANSHA <akansha.test17@gmail.com>
2025-01-09 19:19:13 +0530

    code

---
+++ .venv/Masterfolder/lab05.match.py
@@ -0,0 +1,8 @@
+name=input("enter the name\n")
+match name:
+    case "rajan":
+        print("execute the rajan code")
+    case "bittu":
+        print("execute the bittu code")
+    case _:
+        bittprint("not found")
