b1e8307b5a4eab13b942fbb59d42f752d1c0949b
AKANSHA <akansha.test17@gmail.com>
2025-01-09 15:28:32 +0530

    1stcode

---
+++ .venv/Masterfolder/lab03.py
@@ -0,0 +1,15 @@
+i=0
+while i<10:
+    i=i+1
+    if i==6:
+        print("six")
+    else:
+        print(i)
+
+
+for i in range(0,10):
+    if i==6:
+        print("six")
+    else:
+        print(i)
+
